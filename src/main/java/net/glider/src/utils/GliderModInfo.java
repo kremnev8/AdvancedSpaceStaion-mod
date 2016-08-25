aecad794e4f68a1ec1069e06abdfd86761179fe6
kremnev8 <kremnev8super@yandex.ru>
2016-08-25 15:01:50 +0300

    domain change and massive renamings. no backwards compact

--- src/main/java/net/glider/src/utils/GliderModInfo.java
+++ src/main/java/net/glider/src/utils/GliderModInfo.java
@@ -4,7 +4,7 @@ package net.glider.src.utils;
 public class GliderModInfo {
 	public static final String MOD_ID = "AdvancedSSMod";
 	public static final String MOD_NAME = "Advanced Space Staion mod";
-	public static final String ModTestures = "advSS";
+	public static final String ModTestures = MOD_ID;
 	public static final String Version = "@VERSION@";
 	public static final String ChannelName = "AdvSS";
 }
